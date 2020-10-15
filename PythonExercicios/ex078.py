464ab964dc3a86f40507560d9e6b8502368302da
Rafael Camargo <rafaelcamargo800@hotmail.com>
2020-10-15 19:18:08 -0300

    exercicios python

--- PythonExercicios/ex078.py
+++ PythonExercicios/ex078.py
@@ -1,5 +1,24 @@
 valores = []
+maior = 0
+menor = 0
 for c in range(0, 5):
     valores.append(int(input('Digite um número: ')))
-    for c in enumerate(valores):
-        prin
+    if c == 0:
+        maior = menor = valores[c]
+    else:
+        if valores[c] > maior:
+            maior = valores[c]
+        if valores[c] < menor:
+            menor = valores[c]
+print('Os valores digitados foram: ', end='')
+print(valores)
+for index, v in enumerate(valores):
+    if v == maior:                                                     ###### ARRUMAR ########3
+        print('{}...'.format(index))
+    if v == menor:
+        print('{}...'.format(index))
+print('O MAIOR número encontrado foi {} e ele está na {}ª posição'.format(maior, index))
+print('O MENOR número encontrado foi {} e ele está na {}ª posição'.format(menor, index)
+
+
+
