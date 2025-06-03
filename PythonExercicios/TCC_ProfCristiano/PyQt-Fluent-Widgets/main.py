1d4398435ef9503daa56a64c4c112d51ed1bf84a
RuhtraX <RuhtraX@users.noreply.github.com>
2025-06-03 14:38:07 -0300

    Dialog para Cadastrar peça
    Definir janela para um tamanho fixo

--- PythonExercicios/TCC_ProfCristiano/PyQt-Fluent-Widgets/main.py
+++ PythonExercicios/TCC_ProfCristiano/PyQt-Fluent-Widgets/main.py
@@ -2,27 +2,47 @@ import sys
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout
-from qfluentwidgets import (PrimaryPushButton)
+from qfluentwidgets import (PrimaryPushButton, LineEdit, MessageBoxBase, SubtitleLabel, PushButton)
+
+class messageBoxCadastrar(MessageBoxBase):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.titleLabel = SubtitleLabel('Cadastrar Peça', self)
+        self.lineEdit = LineEdit(self)
+
+        self.lineEdit.setPlaceholderText('Informe a peça')
+        self.lineEdit.setClearButtonEnabled(True)
+
+        # Adicionar os widgets
+        self.viewLayout.addWidget(self.titleLabel)
+        self.viewLayout.addWidget(self.lineEdit)
+
+        self.yesButton.setText('Cadastrar')
+        self.cancelButton.setText('Cancelar')
+
+        self.widget.setMinimumWidth(350)
 
 class ButtonView(QWidget):
     def __init__(self):
         super().__init__()
         self.setStyleSheet("ButtonView{background: rgb(255,255,255)}")
 
-class PrimaryPushButtonDemo(ButtonView):
+class EstoqueCar(ButtonView):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Estoque Car')
 
         # push button
         self.btnCadastrar = PrimaryPushButton('Cadastar Peça')
+        self.btnCadastrar.clicked.connect(self.showCadastrar)
         self.btnProcurar = PrimaryPushButton('Procurar Peça')
         self.btnAlterar = PrimaryPushButton('Alterar Peça')
         self.btnDeletar = PrimaryPushButton('Deletar Peça')
         self.btnComprar = PrimaryPushButton('Comprar Peça')
         self.btnVender = PrimaryPushButton('Vender Peça')
         self.btnVisualizar = PrimaryPushButton('Visualizar Estoque')
-        self.btnSair = PrimaryPushButton('Sair')
+        self.btnSair = PushButton('Sair')
+        self.btnSair.clicked.connect(self.close)
 
         self.gridLayout = QGridLayout(self)
         self.gridLayout.addWidget(self.btnCadastrar, 0, 0)
@@ -33,8 +53,14 @@ class PrimaryPushButtonDemo(ButtonView):
         self.gridLayout.addWidget(self.btnVender, 5, 0)
         self.gridLayout.addWidget(self.btnVisualizar, 6, 0)
         self.gridLayout.addWidget(self.btnSair, 7, 1)
+        self.setFixedSize(360, 0)
 
-        self.resize(320, 340)
+    def showCadastrar(self):
+        w = messageBoxCadastrar(self)
+        if w.exec():
+            peca = w.lineEdit.text()
+            print(peca.upper())
+
 
 if __name__ == '__main__':
     # enable dpi scale
@@ -45,6 +71,6 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
 
-    w1 = PrimaryPushButtonDemo()
+    w1 = EstoqueCar()
     w1.show()
     app.exec_()
