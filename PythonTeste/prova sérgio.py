657f36b2589daec604ad4a6910ed85bf29008766
RuhtraX <RuhtraX@users.noreply.github.com>
2025-06-03 10:36:23 -0300

    fix prova sérgio

--- PythonTeste/prova sérgio.py
+++ PythonTeste/prova sérgio.py
@@ -1,20 +1,31 @@
-print("1.X-salada = R$ 7,00")
-print("2.Refrigerante = R$ 5,00")
-print("3.Cerveja = R$ 4,00")
-print("4.Misto quante = R$ 3,00")
-if opcao=1;
-    precounit=7
-elif opcao=2
-      precounit=5
-elif opcao=3
-      precounit=4
-elif opcao=4
-      precounit=3
-opcao=input("Escolha um ítem no Menu: ")
-qtde=int(input("Escolha a quantidade: "))
-gorjeta=(precounit*qtde)*0.10
-valor_final=(precounit*qtde)+gorjeta
-print("O valor final com os 10% é igual a {}.".format(valor_final))
+def listarOpcoes(opcoes):
+	for opcao in opcoes:
+		print(f'{opcao} - {opcoes[opcao][0]} ' + 'R${:.2f}'.format(opcoes[opcao][1]))
+	print()
 
+def escolherOpcao(opcoes):
+	while True:
+		try:
+			o = int(input('Escolha um item no Menu: '))
+			if o > 0 and o <= len(opcoes):
+				return o
+			else:
+				print(f'Opção [{o}] inválida.')
+		except Exception as e:
+			print(f'Erro: {e}')
 
-
+if __name__ == '__main__':
+	print('Prova Sérgio\n')
+	opcoes = {
+		1: ['X-salada', 7],
+		2: ['Refrigerante', 5],
+		3: ['Cerveja', 4],
+		4: ['Misto quente', 3]
+	}
+	listarOpcoes(opcoes)
+	opcao = escolherOpcao(opcoes)
+	precounit = opcoes[opcao][1]
+	qtde=int(input("Escolha a quantidade: "))
+	gorjeta=(precounit*qtde)*0.10
+	valor_final=(precounit*qtde)+gorjeta
+	print("O valor final com os 10% é igual a {}.".format(valor_final))
